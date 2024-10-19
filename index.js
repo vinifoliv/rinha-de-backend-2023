ff40efdf4d1d2f1b5d07c5653b6542fb4b8b1f18
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-19 19:17:54 -0300

    POST /pessoas implementado e funcional; Instruções para a rinha adicionadas (créditos a @zanfranceschi); Dockerfile e docker-compose.yml adicionados, mas não preenchidos; Atualização do tabela pessoas em db_table.sql.

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const router = require('./routers/router');
 
 const app = express();
-app.use(cors)
+app.use(router);
 const PORT = 3000;
-
-app.get('/contagem-pessoas', async (req, res) => {
-    
-});
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
